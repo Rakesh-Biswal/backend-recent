23d0b2b6927b15e4771223deaaacb77f9cbc2ffd
sonu4435 <soumyaranjanpraharaj04@gmail.com>
2024-05-23 12:16:40 +0530

    all

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "https://664edde8bac6f5b1d41dd4a6-grand-griff.netlify.app/"
+    origin: "*"
   })
 );
 app.use(express.json());
