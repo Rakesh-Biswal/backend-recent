1b57fd7d82a59cdc67ae63740ef43e76e217bf59
Rakesh-Biswal <rb2306114@gmail.com>
2024-05-24 12:40:48 +0530

    NB

--- models/User.js
+++ models/User.js
@@ -1,37 +1,14 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  phone: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  ip: {
-    type: String,
-    required: true
-  },
-  coins: {
-    type: Number,
-    default: 0
-  },
-  linkStatus: {
-    type: [Boolean],
-    default: () => Array(10).fill(false) // Assuming 10 links
-  }
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  phone: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  ip: { type: String, required: true },
+  coins: { type: Number, default: 0 },
+  linkStatus: { type: Array, default: [] }
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
