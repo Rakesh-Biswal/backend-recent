3c2236f9dba27b95145367c558fd4df28e011c24
Rakesh-Biswal <rb2306114@gmail.com>
2024-05-24 16:55:39 +0530

    jklvbhj

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   phone: { type: String, required: true },
-  email: { type: String, required: true},
+  email: { type: String, required: true,unique: false},
   password: { type: String, required: true },
   ip: { type: String, required: true },
   coins: { type: Number, default: 0 },
