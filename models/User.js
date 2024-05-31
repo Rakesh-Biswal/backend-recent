d83e7fe570ee7c92e390b643baa4a652cfd4d2da
Rakesh-Biswal <rb2306114@gmail.com>
2024-05-31 11:09:45 +0530

    er

--- models/User.js
+++ models/User.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    phone: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    ip: { type: String, required: true, unique: true },
     coins: { type: Number, default: 0 },
     referralId: { type: String, unique: true },
     referrals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
