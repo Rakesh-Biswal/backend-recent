30549ce13afdf4997e00a674106ab9972a3a3e0e
Rakesh-Biswal <rb2306114@gmail.com>
2024-06-14 12:51:34 +0530

    nouuu

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
   })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log('MongoDB connection error:', err));
