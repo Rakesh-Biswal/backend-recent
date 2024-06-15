a561580dd3a710af493ff8de40ade4bdb747a516
Rakesh-Biswal <rb2306114@gmail.com>
2024-06-15 09:01:57 +0530

    bbbbbbb

--- models/Link.js
+++ models/Link.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const linkSchema = new mongoose.Schema({
   url: { type: String, required: true },
   imageUrl: { type: String, required: true },
+  index: { type: Number, required: true, unique: true }, // Unique index for each link
 });
 
 const Link = mongoose.model('Link', linkSchema);
